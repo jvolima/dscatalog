b547047f73941b68c111557c5134145c0abc28e1
João Vitor <jvolima2004@gmail.com>
2023-07-29 16:14:11 -0300

    feat: delete product

--- backend/src/main/java/com/jvolima/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/jvolima/dscatalog/services/ProductService.java
@@ -3,6 +3,8 @@ package com.jvolima.dscatalog.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jvolima.dscatalog.dto.ProductDTO;
 import com.jvolima.dscatalog.entities.Product;
 import com.jvolima.dscatalog.repositories.ProductRepository;
+import com.jvolima.dscatalog.services.exceptions.DatabaseException;
 import com.jvolima.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,5 +34,15 @@ public class ProductService {
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Product not found."));
 		
 		return new ProductDTO(entity, entity.getCategories());
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found.");
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation.");
+		}
 	}
 }
