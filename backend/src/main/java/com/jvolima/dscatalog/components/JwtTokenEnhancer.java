55c1d254f851d8e2cbb30cdb03bca316d32308ab
João Vitor <jvolima2004@gmail.com>
2023-08-13 20:02:58 -0300

    feat: add additional information to jwt token

---
+++ backend/src/main/java/com/jvolima/dscatalog/components/JwtTokenEnhancer.java
@@ -0,0 +1,33 @@
+package com.jvolima.dscatalog.components;
+
+import com.jvolima.dscatalog.entities.User;
+import com.jvolima.dscatalog.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenEnhancer implements TokenEnhancer {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+        User user = userRepository.findByEmail(authentication.getName());
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("userId", user.getId());
+        map.put("userFirstName", user.getFirstName());
+
+        DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
+        token.setAdditionalInformation(map);
+
+        return accessToken;
+    }
+}
