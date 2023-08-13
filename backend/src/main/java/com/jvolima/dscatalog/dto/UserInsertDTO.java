c8666525f52dfee657b7274359158e4e64437156
João Vitor <jvolima2004@gmail.com>
2023-08-12 22:58:35 -0300

    feat: custom constraint validator

--- backend/src/main/java/com/jvolima/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/jvolima/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.jvolima.dscatalog.dto;
 
+import com.jvolima.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 	
--- backend/src/main/java/com/jvolima/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/jvolima/dscatalog/repositories/UserRepository.java
@@ -7,5 +7,5 @@ import com.jvolima.dscatalog.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	User findByEmail(String email);
 }
